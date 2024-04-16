87f64e1da446870aab1d4034f59c934c63e7834f
vticr7 <vti9227@gmail.com>
2024-04-16 21:54:25 +0530

    Initial Commit

---
+++ DashboardApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DashboardApp
+//
+//  Created by Vaibhav  Tiwary on 16/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
